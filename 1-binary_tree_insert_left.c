513fb77547eee38196a26c2d8b675ab1f1195c4a
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-01-30 15:13:11 +0100

    Insert Left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,10 +15,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new = binary_tree_node(parent, value);
-	
+
 	if (!new)
-	       return (NULL);
-	
+		return (NULL);
 	if (parent->left)
 	{
 		new->left = parent->left;
@@ -28,5 +27,3 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	return (new);
 }
-	
-

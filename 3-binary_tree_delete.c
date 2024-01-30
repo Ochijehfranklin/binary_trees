222d688286662bb0fc5e90700ab5f1d15db7dffd
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-01-30 23:23:48 +0100

    Delete

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,4 +1,4 @@
-include "binary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_delete - deletes entire binary tree
@@ -6,6 +6,7 @@ include "binary_trees.h"
  * Return: void
  */
 
+void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree)
 	{

d02a214fcfc2fe593c4d0a28a16bfbaa28ffde4a
Ochijehfranklin <ochijehfranklin@gmail.com>
2024-01-30 21:47:23 +0100

    New Node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,9 +13,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
-	if (!new)
+	if (new == NULL)
+	{
 		return (NULL);
-		
+	}
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
